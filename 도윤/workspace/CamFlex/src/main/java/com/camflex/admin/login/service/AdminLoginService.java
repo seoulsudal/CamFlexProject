4037622c5afac204acf2273d1edf7b3a2ecb08fc
sudal <gusdydwn7@naver.com>
2022-01-14 10:50:26 +0900

    캠플렉스 최종

--- 도윤/workspace/CamFlex/src/main/java/com/camflex/admin/login/service/AdminLoginService.java
+++
@@ -1,9 +0,0 @@
-package com.camflex.admin.login.service;
-
-import com.camflex.client.login.vo.LoginVO;
-
-public interface AdminLoginService {
-
-	public LoginVO loginCheck(LoginVO vo)throws Exception;
-
-}
--- 형민/camflex/src/main/java/com/camflex/admin/login/dao/AdminLoginDAO.java
+++
@@ -1,9 +0,0 @@
-package com.camflex.admin.login.dao;
-
-import com.camflex.client.login.vo.LoginVO;
-
-public interface AdminLoginDAO {
-
-	public LoginVO loginCheck(LoginVO vo)throws Exception;
-
-}
